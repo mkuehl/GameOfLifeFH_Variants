599f4a5e2d3b23b3e479ad73f53396a0cd32ce7a
mkuehl <maxmimilianlkuehl@web.de>
2015-10-27 22:12:15 +0100

    Variant30

--- GODLModel.java
+++ GODLModel.java
@@ -121,5 +121,26 @@ class  GODLModel  extends ModelObservable {
 		notifyObservers();
 	}
 
+		
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(playground);
+		return sb.toString();
+	}
+
+	
+	
+	public boolean equals(Object o) {
+		if(o == null) {
+			return false;
+		} else if(o instanceof GODLModel) {
+			GODLModel ogm = (GODLModel) o;
+			return playground.equals(ogm.playground) && rules.equals(ogm.rules);
+		} else {
+			return false;
+		}
+	}
+
 
 }
